02b9ab5e603edeac9dfcb287fc1d88baa918948c
RetzKu <mikakaunisto@kamk.fi>
2018-05-30 01:57:54 +0300

    -

--- RetuEngine/UniformBuffer.cpp
+++ RetuEngine/UniformBuffer.cpp
@@ -25,16 +25,9 @@ namespace RetuEngine
 	}
 	void UniformBuffer::Update(const VkDevice* logicalDevice, const VkExtent2D &swapChainExtent)
 	{
-		static auto startTime = std::chrono::high_resolution_clock::now();
-		auto currentTime = std::chrono::high_resolution_clock::now();
-
-		float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
-
 		UniformBufferObject ubo = {  };
-		//ubo.Model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f,0.0f, 0.0f));
 		ubo.Model = glm::rotate(glm::mat4(1.0f), glm::radians(-90.0f) , glm::vec3(1.0f, 0.0f, 0.0f));
 		//glm::lookat(worldspace of eye,point eye is looking at, which way is upwards)
-		//ubo.view = glm::lookAt(glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		ubo.view = glm::lookAt(camera->cameraPos,  camera->cameraPos + camera->cameraFront, camera->cameraUp);
 		ubo.proj = glm::perspective(glm::radians(90.0f), swapChainExtent.width / (float)swapChainExtent.height, 0.1f, 10000.0f);
 		ubo.proj[1][1] *= -1; //flipping y coordinate?
